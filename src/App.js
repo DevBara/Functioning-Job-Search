35cf5e8642f1e8a634cf9aa3bf79d3c83140747c
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-15 15:22:10 -0500

    import signIn component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import SignIn from './components/SignIn'
+
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <SignIn/>
     
     </div>
   );
