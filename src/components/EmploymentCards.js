840b14a2cc9083eadb48c3f054ed37535f0684d7
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-05-16 18:19:20 -0500

    more updates. adding new cards works

--- src/components/EmploymentCards.js
+++ src/components/EmploymentCards.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react'
 
 export default class EmploymentCards extends Component {
+   
     render() {
-        constructor(props){
-            super(props);
-
-            this.state({
-                buttonDisplay: 'none'
-            });
-        }
         return (
             <div className="employment-card-wrap">
                 <div className="title-wrap">{this.props.title}</div>
                 <div className="company-dates-wrap">
                     <div className="company">{this.props.company}</div>
-                    <div className="dates">{this.property.dates}</div>
+                    <div className="dates">{this.props.dates}</div>
                 </div>
                 <div className="duties-wrap">
                     <div className="duties">{this.props.duties}</div>
