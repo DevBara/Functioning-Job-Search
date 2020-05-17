4853b8dde3ad6d4bb0faf6535574f20997b74d47
Olga_Sanni <dev.sannikov@gmail.com>
2020-05-17 18:07:49 -0400

    working job search component  and map search component

--- src/components/SearchJobs.js
+++ src/components/SearchJobs.js
@@ -1,85 +1,73 @@
-// import React from "react";
-// import axios from "axios";
+import React from "react";
+import axios from "axios";
+import removemd from "remove-markdown";
+import MapSearch from "./MapSearch"
 
-// class SearchJobs extends React.Component{
-//     constructor(props){
-//         super(props);
-//         this.state={
-//             readMore:"",
-//             jobsList:[]
-//         }
-//     }
 
-// //     availaiable(){
-// //         this.setState({availaiable: true});
-// //            console.log(this.state.availaiable);
-// //     }
+class SearchJobs extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            jobsList:[]
+        }
+    }
 
-//    async getJobs(){
-//         const key=process.env.REACT_APP_API_KEY_JOB;
-//         const appId=process.env.REACT_APP_APP_ID_ID;
-//        try{
-//            let jobsInfo=await axios.get(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${appId}&app_key=${key}&results_per_page=${this.props.results_per_page}&what=${this.props.what}&title_only=${this.props.what}&where=${this.props.where}&distance=${this.props.distance}`)
-//            this.setState({jobsList: jobsInfo.data.results});
-//             console.log(this.state.jobsList);
-//             if(this.state.jobsList!=undefined){
-//                 this.availaiable();}
-            
-//        }
-//        catch(error){
-//            console.log(error);
-//        }
-//     }
-//     componentDidMount(){
-//         // const key=process.env.REACT_APP_API_KEY_JOB;
-//         // const appId=process.env.REACT_APP_APP_ID_ID;
-// this.getJobs();
-// // axios.get(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${appId}&app_key=${key}&results_per_page=${this.props.results_per_page}&what=${this.props.what}&title_only=${this.props.what}&where=${this.props.where}&distance=${this.props.distance}`)
-// // .then(res=>{this.setState({jobsList: res.data.results})},(error) => {
-// //     console.log(error)})
+    async getJobs(){
+        const key=process.env.REACT_APP_API_KEY_JOB;
+        const appId=process.env.REACT_APP_APP_ID_ID;
+       try{
+           let jobsInfo=await axios.get(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${appId}&app_key=${key}&results_per_page=${this.props.results_per_page}&what=${this.props.what}&title_only=${this.props.what}&where=${this.props.where}&distance=${this.props.distance}&content-type=application/json`)
+           this.setState({jobsList: jobsInfo.data.results});
+           console.log(this.state.jobsList)
 
-// }           
-    
+       }
+       catch(error){
+           console.log(error);
+       }
+    }
 
-//     getDescription(id){
-//           // const key=process.env.REACT_APP_API_KEY_JOB;
-//         // const appId=process.env.REACT_APP_APP_ID_ID;
-//         // axios.get(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${appId}&app_key=${key}&results_per_page=${this.props.results_per_page}&what=${this.props.what}&title_only=${this.props.what}&where=${this.props.where}&distance=${this.props.distance}`)
-//         // .then(res=>{this.setState({jobsList: res.data.results})},(error) => {
-//         //     console.log(error)})
-//     }
+    componentDidMount(){
+        this.getJobs();
 
-//     checkIfExist(){
-//             if(this.state.jobsList===undefined){
+}           
 
-//             }
-//             else{
-//             let jobs=this.state.jobsList.map((res)=> {
-//                 return <div>
-//                     <p>{res.title}</p>
-//                     <button type="button" onClick={this.getDescription(res.id)}>Read Description</button>
-//                 </div> 
-//             })
-//             return <div>{jobs}</div>
-//             }
-//         }
+
+    checkIfExist(){
+            if(this.state.jobsList===undefined){
+
+            }
+            else{
+            let jobs=this.state.jobsList.map((res)=> {
+                let create=new Date(res.created)
+                let dateCreated=create.getUTCMonth() + "/" +create.getUTCDay() + "/" +create.getUTCFullYear() + "at" + create.getUTCHours() + ":" + create.getUTCMinutes();
+                return <div>
+                   <p>Title: {removemd(res.title)}</p> {removemd(res.title)}
+                    <p>Category: {res.category.label}</p>
+                    <p>Company Name: {res.company.display_name}</p>
+                    <p>Contract time: {res.contract_time}</p>
+                    <p>Description: {removemd(res.description)}</p>
+                    <a href={res.redirect_url}>Aditional information </a>
+                    <p>Created on: {dateCreated}</p>
+                    <div> <MapSearch lat={res.latitude} lng={res.longitude}/></div>
+                    <div>**********************</div>
+                </div> 
+            })
+            return <div>{jobs}</div>
+            }
+        }
         
 
-//     render(){
+    render(){
            
-//         return(
-//             // <ul>
-//             //     {this.state.jobsList.map(id=><li>{id.id}</li>)}
-//             // </ul>
+        return(
 
-
-//             <div>
-//             { this.checkIfExist()}
+            <div>
+            { this.checkIfExist()}
+               <div>
                
-            
-//  {/* <MapSearch lat={this.state.jobsList.results[1].latitude} long={this.state.jobsList.results[1].longitude} /> */}
-//             </div>
-//         )
-//     }
-// }
-// export default SearchJobs;
+               </div>
+         </div>
+        )
+    }
+}
+export default SearchJobs;
