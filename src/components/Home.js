dae0000c966bac1510f323849c4b4560e778f8c8
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-17 12:26:34 -0500

    test data

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export default class Home extends Component {
         return (
             <div>
                 <h3> TEST TEST TEST</h3>
+                <h1> This is the home page</h1>
             </div>
         )
     }
