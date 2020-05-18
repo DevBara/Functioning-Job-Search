652d087fed49bfc9028761ebc8a16e1454aa3b31
Olga_Sanni <dev.sannikov@gmail.com>
2020-05-18 15:58:47 -0400

    Navigation scss

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -56,19 +56,19 @@ render(){
                     <form id="search-form" onSubmit={this.submitButtonHandler}>
                         <div id="form-inputs-div">
                         <div className="form-mini-divs">
-                        <p id="job-title-p">Title:</p>
+                        <p className="job-display-p">Title:</p>
                         <input className="input-class" id="job-title-input"  type="text" value={this.state.searchTitle} onChange={this.getSearchTitle} placeholder="Enter title"/>
                         </div>
                         <div className="form-mini-divs">
-                        <p id="job-location-p">Location:</p>
+                        <p className="job-display-p">Location:</p>
                         <input className="input-class" id="job-location-input"  type="text" value={this.state.searchLocation} onChange={this.getSearchLocation} placeholder="Enter location"/>
                        </div>
                         <div className="form-mini-divs">
-                        <p id="job-distance-p">Search distance:</p>
+                        <p className="job-display-p">Search distance:</p>
                         <input className="input-class" id="job-distance-input"  type="text" value={this.state.searchDistanceMl} onChange={this.getSearchDistanceKm} placeholder="Enter searching distance in miles"/>
                        </div>
                         <div className="form-mini-divs">
-                        <p id="job-display-p">Number of results to display:</p>
+                        <p className="job-display-p">Number of results:</p>
                         <select id="select-to-display" value={this.state.value} onChange={this.getNumberToDisplay}>
                           <option className="options-to-display-class" value="5">5</option>
                           <option className="options-to-display-class" value="10">10</option>
@@ -93,35 +93,25 @@ render(){
                          </select>
                         </div>
                         <div className="form-mini-divs">
-                        <p id="job-display-p">Number of results to display:</p>
+                        <p className="job-display-p">Page to get information from:</p>
                         <select id="select-to-display" value={this.state.value} onChange={this.getNumberPage}>
-                          <option className="options-to-display-class" value="5">5</option>
+                          <option className="options-to-display-class" value="1">1</option>
+                          <option className="options-to-display-class" value="2">2</option>
+                          <option className="options-to-display-class" value="3">3</option>
+                          <option className="options-to-display-class" value="4">4</option>
+                          <option className="options-to-display-class" value="5">5</option> 
+                          <option className="options-to-display-class" value="6">6</option>
+                          <option className="options-to-display-class" value="7">7</option>
+                          <option className="options-to-display-class" value="8">8</option>
+                          <option className="options-to-display-class" value="9">9</option>
                           <option className="options-to-display-class" value="10">10</option>
-                          <option className="options-to-display-class" value="15">15</option>
-                          <option className="options-to-display-class" value="20">20</option>
-                          <option className="options-to-display-class" value="25">25</option> 
-                          <option className="options-to-display-class" value="30">30</option>
-                          <option className="options-to-display-class" value="35">35</option>
-                          <option className="options-to-display-class" value="40">40</option>
-                          <option className="options-to-display-class" value="45">45</option>
-                          <option className="options-to-display-class" value="50">50</option>
-                          <option className="options-to-display-class" value="55">55</option>
-                          <option className="options-to-display-class" value="60">60</option> 
-                          <option className="options-to-display-class" value="65">65</option> 
-                          <option className="options-to-display-class" value="70">70</option>
-                          <option className="options-to-display-class" value="75">75</option>
-                          <option className="options-to-display-class" value="80">80</option>
-                          <option className="options-to-display-class" value="85">85</option>  
-                          <option className="options-to-display-class" value="90">90</option> 
-                          <option className="options-to-display-class" value="95">95</option>
-                          <option className="options-to-display-class" value="100">100</option>
-                         </select>
+                        </select>
                         </div>
                         </div>
                         <div id="form-buttons-divs">
-                        <input className="submit-button-form" type="submit" />
+                        <input id="submit-button-form" type="submit" />
                         {/* clear button will clear the state, put values to initial state in order to do a new search */}
-                        <button className="clear-button-search-page" type="button" onClick={()=>{this.setState({searchTitle: "", searchLocation: "", searchDistanceMl: "", search: false})}}>Clear</button>
+                        <button id="clear-button-search-page" type="button" onClick={()=>{this.setState({searchTitle: "", searchLocation: "", searchDistanceMl: "", search: false})}}>Clear</button>
                         </div>
                         <p id="clear-text">If you want to start a new search please press clear</p>
                     </form>
