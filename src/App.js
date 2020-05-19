2a2a9344a386938434545d283e0ec622c0dbdad3
DevBara <61251493+DevBara@users.noreply.github.com>
2020-05-18 21:11:52 -0500

    removed home component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import UserProvider from './providers/UserProvider'
 import Application from "./components/Application";
 import SearchPage from "./components/SearchPage"
-import Home from './components/Home';
 import ProfilePage from "./components/ProfilePage";
 import "./styles/Navigation.scss"
 import "./styles/ProfilePage.scss"
@@ -19,9 +18,6 @@ function App() {
           <nav className="nav-bar">
             <ul id="nav-ul">
               <li>
-                <Link className="link" to ="/home"> Home</Link>
-              </li>
-              <li>
                 <Link className="link" to ="/searchpage"> Search Jobs</Link>
               </li>
               <li>
@@ -30,7 +26,6 @@ function App() {
             </ul>
           </nav>
           <Switch>
-            <Route path="/Home" component={Home} />
             <Route path="/SearchPage" component={SearchPage} />
             <Route path="/profile" component={ProfilePage} />
           </Switch>
