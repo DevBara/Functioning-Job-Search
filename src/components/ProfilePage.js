0c518979ab0c6b5a482c725fe30225f4d8c15fe3
Student_M9 <Student_M9@PS-DAL-M9.local>
2020-05-18 22:11:29 -0500

    delete button

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -4,14 +4,46 @@ import UserDetails from './UserDetails'
 import UserPhoto from './UserPhoto'
 
 export default class ProfilePage extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            createButton: 'block',
+            deleteButton: 'none',
+            profileDisplay: 'none'
+        }
+
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+   
+    handleDelete(e) {
+        this.setState({
+            deleteButton: 'none',
+            createButton: 'block',
+            profileDisplay: 'none'
+        });
+    }
+    handleCreate(e) {
+
+        this.setState({
+            deleteButton: 'block',
+            createButton: 'none',
+            profileDisplay: 'flex'
+        });
+    }
+
     render() {
         return (
             <div id="profile-wrap">
-                <div id="upper-profile-section">
+                <button className="profile-buttons" id="delete-button" style={{display:this.state.deleteButton}} onClick={this.handleDelete}>Delete Profile</button>
+                <button className="profile-buttons" id="create-button" style={{display:this.state.createButton}} onClick={this.handleCreate}>Create Profile</button> 
+                <div id="upper-profile-section" style={{display:this.state.profileDisplay}}>
                     <UserPhoto />
                     <UserDetails />
                 </div>
-                <div id="lower-profile-section">
+                <div id="lower-profile-section" style={{display:this.state.profileDisplay}}>
                     <EmploymentHistory />
                 </div>
             </div>
